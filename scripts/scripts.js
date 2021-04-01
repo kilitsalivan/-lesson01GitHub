131f6460084cccd257c0835861e1d3889c1dc65a
Aleksandr Selivanov <salivan_s@mail.ru>
2021-04-01 18:33:27 +0300

    ыторой комит

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
-alert('hello');
-
 let money = 20;
 let income = 'Добрый вечер';
 let addExpenses = 2+2;
 let deposit = 1;
 let mission = 23;
 let period = 'zxcxc'; 
+alert('Шома ва');
+alert('Шома ва2');
