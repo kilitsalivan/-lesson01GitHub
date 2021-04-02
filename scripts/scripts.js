e828eb2b203aff2fd16593d70bcd349920989da3
Aleksandr Selivanov <salivan_s@mail.ru>
2021-04-02 16:59:25 +0300

    урок 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,17 @@
-let money = 20;
-let income = 'Добрый вечер';
-let addExpenses = 2+2;
-let deposit = 1;
-let mission = 23;
-let period = 'zxcxc'; 
-alert('Шома ва');
-alert('Шома ва2');
-console.log('снег');
+let money = 50000;
+let income = 'Фриланс';
+let addExpenses = 'Комуналка, покупки, такси, интернет';
+let deposit = true;
+let mission = 100000;
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев и цель заработать ${mission} рублей`);
+addExpenses = addExpenses.toLocaleLowerCase();
+addExpenses= addExpenses.split(',');
+console.log(addExpenses);
+let budgetDay =  money/30;
+console.log(budgetDay);
